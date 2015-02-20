b8845428d9339130144d54af8002729d3d967a08
Eric Fowler <ericfowler303@gmail.com>
2015-02-20 12:12:24 -0700

    Initial Commit

---
+++ Week3CodeChallenge/Week3CodeChallenge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Week3CodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        /// <summary>
+        /// A function that calculates the starting value of the longest Collatz sequence.
+        /// This function will start at 1 million and look at which value generates the
+        /// longest sequence.
+        /// </summary>
+        /// <returns>Starting value of the longest sequence</returns>
+        public static long LongestCollatzSequence()
+        {
+            return 0; // Default return value, replace this
+        }
+
+        /// <summary>
+        /// A Function that adds up each even number in a Fibonacci Sequence until the maxValue
+        /// then prints the sum of those numbers to the console
+        /// </summary>
+        /// <param name="maxValue"></param>
+        /// <returns>The sum of every even number for the given number of Fibonacci digits</returns>
+        public static long EvenFibonacciSequencer(long maxValue)
+        {
+            return 0; // Default return value, replace this
+        }
+
+        /// <summary>
+        /// Function that finds the n'th prime indicated by the parameter
+        /// </summary>
+        /// <param name="maxPrime"></param>
+        /// <returns>The prime number which is the n'th found</returns>
+        public static int FindNPrimes(int maxPrime)
+        {
+            return 0; // Default return value, replace this
+        }
+    }
+}
---
+++ Week3CodeChallenge/Week3CodeChallenge/Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Week3CodeChallenge
+{
+    [TestFixture]
+    class Test
+    {
+        [Test, Timeout(10000)]
+        public void testFindNPrimes6()
+        {
+            Assert.AreEqual(13,Program.FindNPrimes(6));
+        }
+        [Test, Timeout(10000)]
+        public void testFindNPrimes10001()
+        {
+            Assert.AreEqual(104743,Program.FindNPrimes(10001));
+        }
+        [Test, Timeout(10000)]
+        public void testCollatz()
+        {
+            Assert.AreEqual(837799, Program.LongestCollatzSequence());
+        }
+        [Test, Timeout(10000)]
+        public void testEvenFibbonacciSequence5()
+        {
+            Assert.AreEqual(2, Program.EvenFibonacciSequencer(5));
+        }
+        [Test, Timeout(10000)]
+        public void testEvenFibbonacciSequence50()
+        {
+            Assert.AreEqual(44, Program.EvenFibonacciSequencer(50));
+        }
+    }
+}
